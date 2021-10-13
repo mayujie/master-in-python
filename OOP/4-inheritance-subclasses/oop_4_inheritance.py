9054cec7074b42bb485d12a125c0b4b19fba07ea
yujie_ma <yujiema@tcl.com>
2021-10-13 09:32:20 +0200

    add oop inheritance

---
+++ OOP/4-inheritance-subclasses/oop_4_inheritance.py
@@ -0,0 +1,69 @@
+class Employee:
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        Employee.__init__(self, first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees: list = None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Corey', 'Schafer', 50000, 'Python')
+dev_2 = Developer('Test', 'Employee', 60000, 'Java')
+
+print(dev_1.email)
+print(dev_2.prog_lang)
+# print(help(Developer))
+
+mgr_1 = Manager('Sue', 'Smith', 90000, [dev_1])
+print(mgr_1.email)
+
+mgr_1.add_emp(dev_2)
+mgr_1.print_emps()
+
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emps()
+
+print(isinstance(mgr_1, Manager)) # True
+print(isinstance(mgr_1, Employee)) # True
+print(isinstance(mgr_1, Developer)) # False
+
+print(issubclass(Developer, Employee)) # True
+print(issubclass(Manager, Employee)) # True
+print(issubclass(Manager, Developer)) # False
